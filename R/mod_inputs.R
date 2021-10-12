591f38a4b49480f36a1dfffb4e56e03794af5716
mhdzy <mhzyxw@gmail.com>
2021-10-11 20:35:00 -0400

    feat: add play/poop popups

--- R/mod_inputs.R
+++ R/mod_inputs.R
@@ -16,6 +16,7 @@ mod_inputs_ui <- function(id){
     icon = f7Icon("calendar_badge_plus"),
     active = TRUE,
 
+    ## date picker ----
     f7DatePicker(
       inputId = ns("date"),
       label = "today's date",
@@ -25,6 +26,7 @@ mod_inputs_ui <- function(id){
       closeByOutsideClick = TRUE
     ),
 
+    ## button row ----
     f7Row(
       f7Col(
         f7Button(
@@ -48,8 +50,10 @@ mod_inputs_ui <- function(id){
         )
       )
     ),
+
+    ## food sheet ----
     f7Sheet(
-      id = ns("food_box"),
+      id = ns("food_sheet"),
       label = "food/water",
       orientation = "bottom",
       swipeToClose = TRUE,
@@ -61,7 +65,7 @@ mod_inputs_ui <- function(id){
         f7Row(
           f7Col(
             f7Stepper(
-              inputId = ns("food_dry"),
+              inputId = ns("food"),
               label = NULL,
               min = 0,
               max = 5,
@@ -72,7 +76,7 @@ mod_inputs_ui <- function(id){
           ),
           f7Col(
             f7Button(
-              inputId = ns("food_dry_submit"),
+              inputId = ns("food_submit"),
               label = "submit",
               size = "small",
               color = "teal",
@@ -84,7 +88,7 @@ mod_inputs_ui <- function(id){
         f7Row(
           f7Col(
             f7Stepper(
-              inputId = ns("food_wet"),
+              inputId = ns("water"),
               label = NULL,
               min = 0,
               max = 5,
@@ -95,7 +99,7 @@ mod_inputs_ui <- function(id){
           ),
           f7Col(
             f7Button(
-              inputId = ns("food_wet_submit"),
+              inputId = ns("water_submit"),
               label = "submit",
               size = "small",
               color = "lightblue",
@@ -107,21 +111,174 @@ mod_inputs_ui <- function(id){
         f7Row(
           f7Col(
             f7Button(
-              inputId = ns("cancel"),
+              inputId = ns("food_cancel"),
               label = "cancel",
               color = "gray"
             )
           ),
           f7Col(
             f7Button(
-              inputId = ns("submit"),
+              inputId = ns("food_confirm"),
+              label = "confirm",
+              color = "blue"
+            )
+          )
+        )
+      )
+    ),
+
+    ## play sheet ----
+    f7Sheet(
+      id = ns("play_sheet"),
+      label = "walk/play",
+      orientation = "bottom",
+      swipeToClose = TRUE,
+      swipeToStep = FALSE,
+      closeByOutsideClick = TRUE,
+      backdrop = FALSE,
+      tagList(
+        h4("play"),
+        f7Row(
+          f7Col(
+            f7Stepper(
+              inputId = ns("play"),
+              label = NULL,
+              min = 0,
+              max = 60,
+              step = 5,
+              value = 30,
+              color = "deeppurple"
+            )
+          ),
+          f7Col(
+            f7Button(
+              inputId = ns("play_submit"),
               label = "submit",
+              size = "small",
+              color = "deeppurple",
+              fill = FALSE
+            )
+          )
+        ),
+        h4("walk"),
+        f7Row(
+          f7Col(
+            f7Stepper(
+              inputId = ns("walk"),
+              label = NULL,
+              min = 0,
+              max = 90,
+              step = 5,
+              value = 15,
+              color = "orange"
+            )
+          ),
+          f7Col(
+            f7Button(
+              inputId = ns("walk_submit"),
+              label = "submit",
+              size = "small",
+              color = "orange",
+              fill = FALSE
+            )
+          )
+        ),
+        br(), br(),
+        f7Row(
+          f7Col(
+            f7Button(
+              inputId = ns("play_cancel"),
+              label = "cancel",
+              color = "gray"
+            )
+          ),
+          f7Col(
+            f7Button(
+              inputId = ns("play_confirm"),
+              label = "confirm",
+              color = "blue"
+            )
+          )
+        )
+      )
+    ),
+
+    ## poop sheet ----
+    f7Sheet(
+      id = ns("poop_sheet"),
+      label = "pee/poop",
+      orientation = "bottom",
+      swipeToClose = TRUE,
+      swipeToStep = FALSE,
+      closeByOutsideClick = TRUE,
+      backdrop = FALSE,
+      tagList(
+        h4("pee"),
+        f7Row(
+          f7Col(
+            f7Stepper(
+              inputId = ns("pee"),
+              label = NULL,
+              min = 0L,
+              max = 3L,
+              step = 1L,
+              value = 1L,
+              color = "yellow"
+            )
+          ),
+          f7Col(
+            f7Button(
+              inputId = ns("pee_submit"),
+              label = "submit",
+              size = "small",
+              color = "yellow",
+              fill = FALSE
+            )
+          )
+        ),
+        h4("poop"),
+        f7Row(
+          f7Col(
+            f7Stepper(
+              inputId = ns("poop"),
+              label = NULL,
+              min = 0,
+              max = 3L,
+              step = 1L,
+              value = 1L,
+              color = "deeporange"
+            )
+          ),
+          f7Col(
+            f7Button(
+              inputId = ns("poop_submit"),
+              label = "submit",
+              size = "small",
+              color = "deeporange",
+              fill = FALSE
+            )
+          )
+        ),
+        br(), br(),
+        f7Row(
+          f7Col(
+            f7Button(
+              inputId = ns("potty_cancel"),
+              label = "cancel",
+              color = "gray"
+            )
+          ),
+          f7Col(
+            f7Button(
+              inputId = ns("potty_confirm"),
+              label = "confirm",
               color = "blue"
             )
           )
         )
       )
     )
+
   )
 }
 
@@ -135,7 +292,21 @@ mod_inputs_server <- function(id) {
     observeEvent(
       eventExpr = input$food_btn,
       handlerExpr = {
-        updateF7Sheet("food_box", session)
+        updateF7Sheet("food_sheet", session)
+      }
+    )
+
+    observeEvent(
+      eventExpr = input$play_btn,
+      handlerExpr = {
+        updateF7Sheet("play_sheet", session)
+      }
+    )
+
+    observeEvent(
+      eventExpr = input$poop_btn,
+      handlerExpr = {
+        updateF7Sheet("poop_sheet", session)
       }
     )
 
