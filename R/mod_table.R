2d82f8a3b4cb9da7b390bda1e4afe3b2d1f7bf4e
mhdzy <mhzyxw@gmail.com>
2021-10-31 20:09:39 -0400

    misc: simplify dbi param fetch
    Signed-off-by: mhdzy <mhzyxw@gmail.com>

--- R/mod_table.R
+++ R/mod_table.R
@@ -42,9 +42,7 @@ mod_table_server <- function(id, refresh_pull, refresh_tabs, datetime) {
 
     df_data <- eventReactive(df_data_rt(), {
       log_trace("[{id}] df refresh")
-      # invalidateLater(5000)
-      dbi <- get_golem_options("dbi")
-      dbi$query_self_param("kpthor", "events")
+      get_golem_options("dbi")$query_self_param("kpthor", "events")
     })
 
     output$table <- renderUI({
