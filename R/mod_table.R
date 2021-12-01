11f3d12f0c60e4d6544418bd0ce8da39be5f989f
mhdzy <mhdzy@users.noreply.github.com>
2021-11-30 20:18:00 -0500

    feat: prevent db hit on every tab switch; only when viewing tbl & pull to refresh
    Signed-off-by: mhdzy <mhdzy@users.noreply.github.com>

--- R/mod_table.R
+++ R/mod_table.R
@@ -30,17 +30,22 @@ mod_table_server <- function(id, refresh_pull, refresh_tabs, datetime) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    df_data_rt <- reactive({
-      # force invalidation and update on df_data without hiding the result
-      # when input$ptr (refresh_trigger()) becomes NULL at end of animation
+    refresh <- reactiveVal(0L)
+
+    # no observeEvent because only act on certain conditions
+    observe({
+      # this observer exists to force invalidation and update on df_data
+      # without hiding the result when input$ptr (refresh_tabs()) becomes
+      # NULL at end of animation
       #
-      # also toss in tab switch
-      refresh_pull()
-      refresh_tabs()
-      1L
+      # also react to tab switch, but only when the user is trying to view tbl
+      if (!is.null(refresh_pull()) || refresh_tabs()  == "table") {
+        # need to use isolate to prevent this from observing itself & reacting
+        isolate(refresh(refresh() + 1))
+      }
     })
 
-    df_data <- eventReactive(df_data_rt(), {
+    df_data <- eventReactive(refresh, {
       log_trace("[{id}] df refresh")
       get_golem_options("dbi")$query_self_param_clear("kpthor", "events")
     })
