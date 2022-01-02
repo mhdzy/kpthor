b75b3a299c3947ff06c2fca8f6d20d146dd31bc5
mhdzy <mhdzy@users.noreply.github.com>
2022-01-02 14:41:09 -0500

    feat: rename mod_datetime to mod_appdate
    Signed-off-by: mhdzy <mhdzy@users.noreply.github.com>

--- R/mod_appdate_row.R
+++ R/mod_appdate_row.R
@@ -1,4 +1,4 @@
-#' datetime_row UI Function
+#' appdate_row UI Function
 #'
 #' @description A shiny Module.
 #'
@@ -7,14 +7,15 @@
 #' @noRd
 #'
 #' @importFrom lubridate hour minute today
-#' @importFrom shiny NS tagList
+#' @importFrom shiny NS tagList h5
 #' @importFrom shinyMobile f7Row f7Col f7DatePicker f7Slider f7Icon f7Stepper
-mod_datetime_row_ui <- function(id) {
+mod_appdate_row_ui <- function(id) {
   ns <- NS(id)
 
   tagList(
     f7Row(
       f7Col(
+        width = 6,
         f7DatePicker(
           inputId = ns("date"),
           label = "date",
@@ -30,21 +31,28 @@ mod_datetime_row_ui <- function(id) {
           seq_along(get_golem_options(id)),
           function(x) {
             golem_opts <- get_golem_options(id)
-            tagList(
-              br(),
-              f7Stepper(
-                inputId = ns(paste0("time_", names(golem_opts)[x])),
-                label = paste0("* time (", names(golem_opts)[x], ") *"),
-                min = golem_opts[[x]]$min,
-                max = golem_opts[[x]]$max,
-                value = golem_opts[[x]]$value(Sys.time()),
-                color = golem_opts[[x]]$color,
-                #size = "small",
-                raised = TRUE,
-                wraps = TRUE,
-                manual = TRUE,
-                autorepeat = TRUE,
-                buttonsEndInputMode = TRUE
+            f7Row(
+              f7Col(
+                align = "left",
+                h4(names(golem_opts)[x]),
+              ),
+              f7Col(
+                align = "center",
+                f7Stepper(
+                  inputId = ns(paste0("time_", names(golem_opts)[x])),
+                  label = "",
+                  #label = paste0("* time (", names(golem_opts)[x], ") *"),
+                  min = golem_opts[[x]]$min,
+                  max = golem_opts[[x]]$max,
+                  value = golem_opts[[x]]$value(Sys.time()),
+                  color = golem_opts[[x]]$color,
+                  #size = "small",
+                  raised = TRUE,
+                  wraps = TRUE,
+                  manual = TRUE,
+                  autorepeat = TRUE,
+                  buttonsEndInputMode = TRUE
+                )
               )
             )
           }
@@ -55,12 +63,12 @@ mod_datetime_row_ui <- function(id) {
 
 }
 
-#' datetime_row Server Functions
+#' appdate_row Server Functions
 #'
 #' @noRd
 #'
 #' @importFrom shiny moduleServer reactive
-mod_datetime_row_server <- function(id) {
+mod_appdate_row_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
